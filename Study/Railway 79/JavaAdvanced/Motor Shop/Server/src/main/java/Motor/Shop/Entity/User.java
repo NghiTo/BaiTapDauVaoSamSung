2d7592a5fec83bffe51b318b00de9146a081fc0a
Kenzy <tobanghi@gmail.com>
2024-01-07 22:58:53 +0700

    Create user form

---
+++ Study/Railway 79/JavaAdvanced/Motor Shop/Server/src/main/java/Motor/Shop/Entity/User.java
@@ -0,0 +1,30 @@
+package Motor.Shop.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "User")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "username", length = 20, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 72, nullable = false)
+    private String password;
+
+    @Column(name = "email", length = 50, nullable = false, unique = true)
+    private String email;
+}
+
