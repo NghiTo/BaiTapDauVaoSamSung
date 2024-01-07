ad93119c82320d7186a64c1cf01e9c02efecb335
Kenzy <tobanghi@gmail.com>
2024-01-07 22:39:54 +0700

    Create Server

---
+++ Study/Railway 79/JavaAdvanced/Motor Shop/Server/src/main/java/Motor/Shop/MotorShopApplication.java
@@ -0,0 +1,13 @@
+package Motor.Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorShopApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MotorShopApplication.class, args);
+	}
+}
---
+++ Study/Railway 79/JavaAdvanced/Motor Shop/Server/src/test/java/Motor/Shop/MotorShopApplicationTests.java
@@ -0,0 +1,13 @@
+package Motor.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
