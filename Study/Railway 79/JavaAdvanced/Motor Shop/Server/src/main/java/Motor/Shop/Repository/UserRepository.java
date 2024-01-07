93f8a58ceb10f66fbaddc90baf2abf135acc5f93
Kenzy <tobanghi@gmail.com>
2024-01-07 23:02:12 +0700

    Create user repository

---
+++ Study/Railway 79/JavaAdvanced/Motor Shop/Server/src/main/java/Motor/Shop/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package Motor.Shop.Repository;
+
+import Motor.Shop.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>
+{
+
+}
